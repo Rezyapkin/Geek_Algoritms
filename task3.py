72a2a384ed68d65c6a11997b80bd6e35a1d001bc
Rezyapkin <rezyapkin@yandex.ru>
2021-07-26 19:41:15 +0300

    complete task3

---
+++ task3.py
@@ -0,0 +1,24 @@
+'''
+Задание 3.
+В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+'''
+
+from random import randint
+
+len_array = 10
+max_value = 100
+random_array = [randint(0, max_value) for _ in range(len_array)]
+print(random_array)
+
+min = (0, random_array[0])
+max = (0, random_array[0])
+
+for index, value in enumerate(random_array):
+    if value < min[1]:
+        min = index, value
+    if value > max[1]:
+        max = index, value
+
+random_array[min[0]], random_array[max[0]] = random_array[max[0]], random_array[min[0]]
+
+print(random_array)

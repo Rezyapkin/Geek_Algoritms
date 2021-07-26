616ce392638541b1e1a7e29e86a93560f2d3cc1a
Rezyapkin <rezyapkin@yandex.ru>
2021-07-26 20:30:52 +0300

    complete task6

--- task3.py
+++ task3.py
@@ -10,15 +10,15 @@ max_value = 100
 random_array = [randint(0, max_value) for _ in range(len_array)]
 print(random_array)
 
-min = (0, random_array[0])
-max = (0, random_array[0])
+min_ = (0, random_array[0])
+max_ = (0, random_array[0])
 
 for index, value in enumerate(random_array):
-    if value < min[1]:
-        min = index, value
-    if value > max[1]:
-        max = index, value
+    if value < min_[1]:
+        min_ = index, value
+    if value > max_[1]:
+        max_ = index, value
 
-random_array[min[0]], random_array[max[0]] = random_array[max[0]], random_array[min[0]]
+random_array[min_[0]], random_array[max_[0]] = random_array[max_[0]], random_array[min_[0]]
 
 print(random_array)
